4cc4d6f476b9790052882435335179ee7f8fb9a8
Ygor Kayan <ygorkayan@gmail.com>
2019-11-17 11:26:42 -0300

    Add files via upload

---
+++ links.py
@@ -0,0 +1,19 @@
+import webbrowser
+
+
+links = [
+    "https://get.adobe.com/br/reader/",
+    "https://www.ccleaner.com/defraggler/download",
+    "https://www.ccleaner.com/pt-br/ccleaner/download",
+    "https://www.google.com/intl/pt-BR/chrome/",
+    "https://www.nvidia.com/Download/index.aspx",
+    "https://www.videolan.org/vlc/index.pt-BR.html",
+    "https://www.win-rar.com/predownload.html?&L=9",
+    "https://www.qbittorrent.org/download.php",
+    "https://code.visualstudio.com/",
+    "https://www.python.org/downloads/release/python-380/",
+    "https://www.oo-software.com/en/ooappbuster"
+    ]
+
+for link in links:
+    webbrowser.open(link)
